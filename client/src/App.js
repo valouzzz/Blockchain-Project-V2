d3214b9c993d63382075e0fb34d61a89441f7962
EricTech0 <techwitheric@gmail.com>
2024-06-16 16:13:47 -0700

    2 - remove unused default react functionalities

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { requestAccounts } from "./utils/contractServices";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(async () => {
+    const out = await requestAccounts();
+    console.log("✌️out --->", out);
+  }, []);
+
+  return <>Hello</>;
 }
 
 export default App;
